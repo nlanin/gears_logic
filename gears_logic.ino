90999c85ca78423ff2080fd0e1df633eaf7324b2
nlanin <nlanin@yandex.ru>
2017-07-19 22:44:00 +0300

    Убрал лишние переменные.

--- gears_logic.ino
+++ gears_logic.ino
@@ -1,13 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //pinMode(2, INPUT);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  
+  pinMode(5, INPUT);
+  pinMode(7, INPUT);
+  
+  //pinMode(7, OUTPUT);
+  //pinMode(8, OUTPUT);
   //pinMode(9, OUTPUT); 
   //pinMode(13, OUTPUT);
 }
@@ -16,10 +17,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   //digitalWrite(3, HIGH);
   //digitalWrite(5, HIGH);
-  digitalWrite(4, LOW);
-  digitalWrite(6, LOW);
-  int GearOpened = digitalRead(3);
-  int GearClosed = digitalRead(5);
+  //digitalWrite(4, LOW);
+  //digitalWrite(6, LOW);
+  int GearOpened = digitalRead(5);
+  int GearClosed = digitalRead(7);
 
 
   Serial.print("GearOpened: ");
